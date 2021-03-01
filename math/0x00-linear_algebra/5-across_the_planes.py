d3b8c6dcc02554a89368dc6fe7d3d576c720d66c
Hernan Dario Castaño Rueda <881@holbertonschool.com>
2021-03-01 17:35:49 -0500

    Fix pycodestyle

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,17 +4,20 @@
 
 def add_matrices2D(mat1, mat2):
     """Adds two 2D matrices"""
-    return list(map(lambda x, y: add_arrays(x, y), mat1, mat2)) if shape(mat1) == shape(mat2) else None
+    return list(map(lambda x, y: add_arrays(x, y), mat1, mat2)) \
+        if shape(mat1) == shape(mat2) else None
 
 
 def add_arrays(arr1, arr2):
     """Adds two lists"""
-    return list(map(lambda x, y: x+y, arr1, arr2)) if len(arr1) == len(arr2) else None
+    return list(map(lambda x, y: x+y, arr1, arr2)) \
+        if len(arr1) == len(arr2) else None
 
 
 def shape(matrix):
     """Checks the shape of a matrix"""
     nrows = len(matrix)
-    ncols = len(matrix[0]) if all(list(map(lambda x: len(x) == len(matrix[0]), matrix))) else 0
+    ncols = len(matrix[0]) \
+        if all(list(map(lambda x: len(x) == len(matrix[0]), matrix))) else 0
 
     return (nrows, ncols) if nrows != 0 and ncols != 0 else None
--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -9,11 +9,13 @@ def cat_matrices2D(mat1, mat2, axis=0):
     if axis == 0:
         return m1 + m2 if columns_len(mat1) == len(mat2[0]) else None
     elif axis == 1:
-        return list(map(lambda x, y: x+y, m1, m2)) if columns_len(mat1) == len(mat2) else None
+        return list(map(lambda x, y: x+y, m1, m2)) \
+            if columns_len(mat1) == len(mat2) else None
     else:
         return None
 
 
 def columns_len(matrix):
     """Get the column length of the matrix"""
-    return len(matrix[0]) if all(list(map(lambda x: len(x) == len(matrix[0]), matrix))) else None
+    return len(matrix[0]) \
+        if all(list(map(lambda x: len(x) == len(matrix[0]), matrix))) else None

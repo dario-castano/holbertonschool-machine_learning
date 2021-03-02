7c2abedbc0fb1e06dd310325fff5c424c204bf85
Hernan Dario Castaño Rueda <881@holbertonschool.com>
2021-03-01 19:04:04 -0500

    Fix column_len on task 7

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -10,7 +10,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return m1 + m2 if columns_len(mat1) == len(mat2[0]) else None
     elif axis == 1:
         return list(map(lambda x, y: x+y, m1, m2)) \
-            if columns_len(mat1) == len(mat2) else None
+            if len(mat1) == len(mat2) else None
     else:
         return None
 
--- math/0x00-linear_algebra/7-main.py
+++ math/0x00-linear_algebra/7-main.py
@@ -7,6 +7,13 @@ mat2 = [[5, 6]]
 mat3 = [[7], [8]]
 mat4 = cat_matrices2D(mat1, mat2)
 mat5 = cat_matrices2D(mat1, mat3, axis=1)
+mat6 = [[1]]
+mat7 = [[2]]
+mat8 = cat_matrices2D(mat6, mat7)
+mat9 = cat_matrices2D(mat6, mat7, axis=1)
+mat10 = [[-54, -87, 56, -92, 81], [54, 16, -72, 42, 901]]
+mat11 = [[12, 63], [-10, 69]]
+mat12 = cat_matrices2D(mat10, mat11, axis=1)
 print(mat4)
 print(mat5)
 mat1[0] = [9, 10]
@@ -14,3 +21,6 @@ mat1[1].append(5)
 print(mat1)
 print(mat4)
 print(mat5)
+print(mat8)
+print(mat9)
+print(mat12)

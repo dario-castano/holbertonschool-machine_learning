dc7da04eb82609a228ab84c6bc0c0ce3be22db33
Hernan Dario Castaño Rueda <881@holbertonschool.com>
2021-03-02 21:27:17 -0500

    Fix sum squares task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -2,7 +2,10 @@
 """Task 9. Our life is the sum total of all the decisions we
 make every day, and those decisions are determined by our priorities"""
 
+
 def summation_i_squared(n):
     """Sum of squares"""
-    return sum(list(map(lambda x: x * x, range(1, n+1)))) \
-        if type(n) is int else None
+    if type(n) is int:
+        return sum(list(map(lambda x: x * x, range(1, n+1))))
+    else:
+        return None

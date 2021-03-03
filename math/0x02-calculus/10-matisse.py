b671354202d4c5f1822635412ceb39b2e288ec86
Hernan Dario Castaño Rueda <881@holbertonschool.com>
2021-03-02 21:17:42 -0500

    Add first part of calculus project

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Task 10. Derive happiness in oneself from a good day's work"""
+
+
+def poly_derivative(poly):
+    """Polynomial derivative"""
+    if isa_poly(poly):
+        exponents = list(range(0, len(poly)+1))
+        result = list(map(lambda x, y: x*y, poly, exponents))
+        return result[1:] if len(poly) > 1 else [0]
+    else:
+        return None
+
+
+def isa_poly(poly):
+    """Checks if is a polynomial"""
+    if type(poly) is list and all(list(map(lambda x: isa_number(x), poly))):
+        return True
+    else:
+        return False
+
+
+def isa_number(n):
+    """Checks if is a number"""
+    if type(n) is int or type(n) is float:
+        return True
+    else:
+        return False
---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Task 9. Our life is the sum total of all the decisions we
+make every day, and those decisions are determined by our priorities"""
+
+def summation_i_squared(n):
+    """Sum of squares"""
+    return sum(list(map(lambda x: x * x, range(1, n+1)))) \
+        if type(n) is int else None
